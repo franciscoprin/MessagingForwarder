c305fb08124e6c4aaf6ebff8a3deffd51b18bdb1
francisco prin <franciscoprin@gmail.com>
2021-09-22 15:11:05 -0500

    catching exceptions

--- app/src/main/java/dk/stacktrace/messagingforwarder/HostManager.java
+++ app/src/main/java/dk/stacktrace/messagingforwarder/HostManager.java
@@ -17,29 +17,35 @@ public class HostManager {
 
     public HostnameVerifier getTrustedHost(){
         // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
                 }
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
                 }
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                }
-            }
-        };
+            };
 
-        // Install the all-trusting trust manager
-        SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, trustAllCerts, new java.security.SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-        // Create all-trusting host name verifier
-        HostnameVerifier allHostsValid = new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) {
-                return true;
-            }
-        };
-
-        return allHostsValid;
+            return allHostsValid;
+        }
+        catch (Exception e) {
+            Log.i(TAG, "[HostManager][getTrustedHost] generic error", e);
+            return null;
+        }
     }
 }
